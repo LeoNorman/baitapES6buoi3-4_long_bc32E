b390fb8825aaec8bb844b3ca775de34814edc158
LeoNorman <kairyntvn222@gmail.com>
2022-08-14 20:21:43 +0700

    8.14.2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import { TaskList } from "../models/TaskList.js";
-import { Task } from "../models/Task.js";
+
 
 const addItem = document.querySelector('#addItem')
 
@@ -18,12 +17,12 @@ const renderTaskList = () => {
     const taskList = TaskLists.taskList
     const html = taskList.reduce((value, task) => {
         return (value += `
-        <li>${task.name}
+        <li><span>${task.name}</span>
                             <div class="buttons">
-                                <button class="remove">
+                                <button class="remove" onclick="removeTask('${task.name}')">
                                     <i class="fas fa-trash-alt"></i>
                                 </button>
-                                <button class="complete">
+                                <button class="complete" onclick="addTaskCompleted('${task.name}')">
                                     <i class="far fa-check-circle"></i>
                                     <i class="fas fa-check-circle"></i>
                                 </button>
@@ -34,16 +33,78 @@ const renderTaskList = () => {
     document.querySelector('#todo').innerHTML = html;
 }
 
+const renderTaskCompleted = () => {
+    const taskListCompleted = TaskLists.taskListCompleted
+    const html = taskListCompleted.reduce((value, task) => {
+        return (value += `
+        <li><span>${task.name}</span>
+                            <div class="buttons">
+                                <button class="remove" onclick="removeTaskCompleted('${task.name}')">
+                                    <i class="fas fa-trash-alt"></i>
+                                </button>
+                                <button class="complete">
+                                    <i class="far fa-check-circle"></i>
+                                    <i class="fas fa-check-circle"></i>
+                                </button>
+                            </div>
+                        </li>
+        `)
+    }, '')
+    document.querySelector('#completed').innerHTML = html;
+}
+
+// Lưu ý: Localstore không lưu được method, chỉ lưu được các thuộc tính
+// lưu mảng task xuống local
+const setLocalStore = () => {
+    localStorage.setItem('taskList', JSON.stringify(TaskLists.taskList))
+}
+
+// Get value từ local
+const getLocalStore = () => {
+    const data = localStorage.getItem('taskList')
+    const dataParse = JSON.parse(data) || []
+    console.log(dataParse)
+
+    // sử dụng hàm map => return về 1 mảng mới
+    TaskLists.taskList = dataParse.map((value) => {
+        const task = new Task()
+        for (let key in value) {
+            task[key] = value[key]
+        }
+        return task
+    })
+
+    renderTaskList()
+}
+getLocalStore()
+
 addItem.onclick = () => {
     const task = getFormValues()
     TaskLists.addTask(task)
     getFormValues()
     renderTaskList()
+    setLocalStore()
 }
 
-document.getElementsByClassName('remove').onclick = () => {
-    alert('long')
-    // TaskLists.removeTask(input)
-    // renderTaskList()
+window.removeTask = (name) => {
+    // console.log('long')
+    TaskLists.removeTask(name)
+    renderTaskList()
+    setLocalStore()
+}
+
+window.removeTaskCompleted = (name) => {
+    // console.log('long')
+    TaskLists.removeTaskCompleted(name)
+    renderTaskCompleted()
+    setLocalStore()
+}
+
+window.addTaskCompleted = (name) => {
+    TaskLists.addTaskCompleted(name)
+    renderTaskCompleted()
+    TaskLists.removeTask(name)
+    renderTaskList()
+    console.log(TaskLists.taskListCompleted)
 }
 
--- models/TaskList.js
+++ models/TaskList.js
@@ -1,12 +1,27 @@
-export class TaskList {
+class TaskList {
     constructor() { }
     taskList = []
+    taskListCompleted = []
 
     addTask(task) {
         this.taskList = [...this.taskList, task]
     }
 
+    // addTaskCompletedAfterRemove(task) {
+    //     this.taskListCompleted = this.taskListCompleted.push[task]
+    // }
+
     removeTask(name) {
         this.taskList = this.taskList.filter((task) => task.name !== name)
+    }
+
+    removeTaskCompleted(name) {
+        this.taskListCompleted = this.taskListCompleted.filter((task) => task.name !== name)
+    }
+
+    addTaskCompleted(name) {
+        this.taskListCompleted = [...this.taskList]
+        this.taskListCompleted = this.taskListCompleted.filter((task) => task.name === name )
+        this.taskListCompleted = [...this.taskListCompleted]
     }
 }
